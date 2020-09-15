10f2b7f4c05208f6db72ab184ba63f25fb61c9ee
WangHexie <31768052+WangHexie@users.noreply.github.com>
2020-09-15 12:47:51 +0000

    Update BatchHardTripletLoss.py

--- sentence_transformers/losses/BatchHardTripletLoss.py
+++ sentence_transformers/losses/BatchHardTripletLoss.py
@@ -118,6 +118,10 @@ class BatchHardTripletLoss(nn.Module):
 
         # We put to 0 any element where (a, p) is not valid (valid if a != p and label(a) == label(p))
         anchor_positive_dist = mask_anchor_positive * pairwise_dist
+        
+        print("pairwise_dist", pairwise_dist)
+        print("mask_anchor_positive", mask_anchor_positive)
+        print("anchor_positive_dist", anchor_positive_dist)
 
         # shape (batch_size, 1)
         hardest_positive_dist, _ = anchor_positive_dist.max(1, keepdim=True)
